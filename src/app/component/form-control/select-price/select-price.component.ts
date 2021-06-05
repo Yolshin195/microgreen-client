cfab5c55f8769f27f27f41a7acf42adfd1d790ea
Yolshin <elshin195@gmail.com>
2021-06-05 13:36:30 +0700

    add select price

--- src/app/component/form-control/select-price/select-price.component.ts
+++ src/app/component/form-control/select-price/select-price.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 import { Nomenclature } from 'src/app/service/nomenclature.service';
 import { Price, PriceService } from 'src/app/service/price.service';
@@ -6,24 +7,39 @@ import { Price, PriceService } from 'src/app/service/price.service';
 @Component({
   selector: 'app-select-price',
   templateUrl: './select-price.component.html',
-  styleUrls: ['./select-price.component.css']
+  styleUrls: ['./select-price.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => SelectPriceComponent),
+    multi: true
+  }]
 })
-export class SelectPriceComponent implements OnInit {
+export class SelectPriceComponent implements OnInit, OnChanges, ControlValueAccessor {
   @Input() nomenclature?: Nomenclature;
   propagateChange: Function = () => {};
   propageteTouche: Function = () => {};
   priceList: Price[] = [];
-  inputValue: string = 'Enter title on nomenclature for serch';
+  currentPrice?: Price;
+  devaultInputValue: string = 'Enter title on price for serch';
+  inputValue: string = this.devaultInputValue;
+  isShowList: boolean = false;
 
   constructor(private priceService: PriceService) { }
 
   ngOnInit(): void {
+    console.log("select-price ngOnInit: ", this.nomenclature, this.isNull(this.nomenclature));
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('SelectPrice ngOnChages: ', changes, this.isNull(this.nomenclature));
+    this.getCurrentPrice();
   }
 
   onSelect(price: Price) {
     this.inputValue = this.getTitle(price);
     this.propagateChange(price);
     this.propageteTouche(price);
+    this.onShowList(false);
   }
 
   onSearch(event: any) {
@@ -35,8 +51,54 @@ export class SelectPriceComponent implements OnInit {
       })
   }
 
+  getCurrentPrice() {
+    if (this.nomenclature) {
+      this.priceService.findCurrent(this.nomenclature).subscribe(price => {
+        if (price) {
+          this.currentPrice = price;
+          this.inputValue = this.getTitle(price);
+          this.propagateChange(price);
+          this.propageteTouche(price);
+        } else {
+          this.inputValue = this.devaultInputValue;
+          this.currentPrice = undefined;
+          this.propagateChange(null);
+          this.propageteTouche(null);
+        }
+      });
+    }
+  }
+
   onClick() {
     this.inputValue = '';
+    this.onShowList(true);
+  }
+
+  onToggle() {
+    if (!this.isShowList && this.nomenclature) {
+      this.inputValue = 'Loading...';
+      this.priceService.findAllByNomenclature(this.nomenclature).subscribe(priceList => {
+        this.priceList = priceList;
+        this.inputValue = '';
+        this.onShowList(true);
+      })
+    }
+    else {
+      this.onShowList(!this.isShowList);
+    }
+  }
+
+  onShowList(flag: boolean): void {
+    this.isShowList = flag;
+  }
+
+  onFocusout() {
+    this.onShowList(false);
+    if (this.currentPrice) {
+      this.inputValue = this.getTitle(this.currentPrice);
+    } else {
+      this.inputValue = this.devaultInputValue;
+    }
   }
 
   writeValue(price: Price): void {
@@ -51,7 +113,11 @@ export class SelectPriceComponent implements OnInit {
   }
 
   getTitle(price: Price) {
-    return `${price.nomenclature.title}-${price.price}`;
+    return `${price.price}-${price.create}`;
+  }
+
+  isNull(obj: any) {
+    return !!obj;
   }
 
 }
--- src/app/service/price.service.ts
+++ src/app/service/price.service.ts
@@ -22,8 +22,20 @@ export class PriceService {
     return this.http.get<Price[]>(this.path);
   }
 
+  findAllByNomenclature(nomenclature: Nomenclature) {
+    if (!nomenclature.id) throw Error("В nomenclature нету идентификатора!");
+
+    return this.http.get<Price[]>(`${this.path}/find/nomenclature?id=${nomenclature.id}`);
+  }
+
   save(price: Price): Observable<any> {
     console.log(price);
     return this.http.post(this.path, price);
+  }
+
+  findCurrent(nomenclature: Nomenclature): Observable<Price> {
+    if (!nomenclature.id) throw Error("В nomenclature нету идентификатора!");
+
+    return this.http.get<Price>(`${this.path}/current?id=${nomenclature.id}`);
   }
 }
